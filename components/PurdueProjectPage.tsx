aaeed4108317ae82720616e646dd4662b09f12d9
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2025-06-24 17:47:20 -0600

    Make Purdue page logo white

--- components/PurdueProjectPage.tsx
+++ components/PurdueProjectPage.tsx
@@ -55,7 +55,7 @@ export default function PurdueProjectPage() {
                   alt="Daniel Meier"
                   width={150}
                   height={37}
-                  className="h-9 w-auto brightness-0"
+                  className="h-9 w-auto brightness-0 invert"
                 />
               </button>
             </div>
