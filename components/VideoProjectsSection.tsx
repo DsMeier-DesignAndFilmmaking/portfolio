ad62701d3d92e1969e7fe714f82080ac94474e95
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2025-08-23 12:35:51 -0600

    Remove play button overlay from autoplaying videos in video section

--- components/VideoProjectsSection.tsx
+++ components/VideoProjectsSection.tsx
@@ -131,14 +131,6 @@ export default function VideoProjectsSection() {
                   allowFullScreen
                   className="absolute top-0 left-0 w-full h-full rounded-lg"
                 />
-                {/* Mobile play button overlay */}
-                <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
-                  <div className="bg-black/20 rounded-full p-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-auto">
-                    <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M8 5v14l11-7z"/>
-                    </svg>
-                  </div>
-                </div>
               </div>
             </div>
           ))}
