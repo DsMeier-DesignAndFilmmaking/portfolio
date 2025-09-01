c426b56a3ae4574a685838837012d7b8c0cdfa8e
DsMeier-DesignAndFilmmaking <danmeier@dsmeier.com>
2025-09-01 17:13:42 -0600

    Update bio to include 11+ years of experience

--- app/page.tsx
+++ app/page.tsx
@@ -137,7 +137,7 @@ export default function HomePage() {
                 </div>
                 <div>
                   <p className="font-sf-pro-text text-base sm:text-lg md:text-lg text-gray-900 leading-6 md:leading-7 space-y-2 md:space-y-4 drop-shadow-[0_1px_2px_rgba(255,255,255,0.5)] text-left">
-                  <strong>Hi - I'm Dan</strong>, a systems-thinking Designer & Creative Technologist with a global perspective shaped by experiences in 41 countries. I’m currently exploring how AI can enhance design and digital experiences.
+                  <strong>Hi - I'm Dan</strong>, a systems-thinking Designer & Creative Technologist with 11+ years of experience and a global perspective shaped by experiences in 41 countries. I’m currently exploring how AI can enhance design and digital experiences.
                   </p>
                 </div>
               </motion.div>
